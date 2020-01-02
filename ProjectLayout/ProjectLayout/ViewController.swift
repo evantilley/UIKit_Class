963a295b4032f986f3fe777b64d2ae12993d99c1
Evan Tilley <elt2141@columbia.edu>
2020-01-01 22:04:34 -0500

    Added ProjectLayout
    Simple project demonstrating the usefulness of constraints in UIKit

---
+++ ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Evan Tilley on 1/1/20.
+//  Copyright © 2020 Evan Tilley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
